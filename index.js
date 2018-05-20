fbf995cc04a7561c4eebdf25e10c43ce09348931
Josef Blake <jblake@spotify.com>
2018-05-20 12:30:17 -0400

    added rollup config

---
+++ index.js
@@ -0,0 +1,34 @@
+const babel = require("rollup-plugin-babel");
+const resolve = require("rollup-plugin-node-resolve");
+const path = require("path");
+
+const cwd = process.cwd();
+const pkg = require(`${cwd}/package.json`);
+
+export default {
+  input: require.resolve(path.resolve(cwd, "src")),
+
+  output: [
+    {
+      file: path.resolve(cwd, pkg.main),
+      format: "cjs"
+    },
+
+    pkg.module && {
+      file: path.resolve(cwd, pkg.module),
+      format: "es"
+    }
+  ].filter(Boolean),
+
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.devDependencies || {})
+  ],
+
+  plugins: [
+    resolve(),
+    babel({
+      plugins: [require.resolve("babel-plugin-external-helpers")]
+    })
+  ]
+};
